1681eab7f01cfb5004230d619ead66637c8009f0
artempetrovskiy <artempetrovskiyqa@gmail.com>
2018-08-20 20:59:20 +0300

    functions 2

--- functions.php
+++ functions.php
@@ -6,14 +6,19 @@ function sumTwoNumbers($var1, $var2){
 
 sumTwoNumbers(1, 2);
 
+echo "\n";
 
 
 
+function recurce($a){
+    if ($a < 10) {
+        echo $a . "\n";
+        recurce($a + 1);
+    }
+}
 
 
-
-
-
+recurce(1);
 
 
 
